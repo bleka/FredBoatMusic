8b2170cfc5df6bae70dcd3723c817386e5f264fb
Frederikam <frogkr@gmail.com>
2016-05-27 22:51:08 +0200

    Added incomplete play command

---
+++ src/main/java/fredboat/audio/PlayerRegistry.java
@@ -0,0 +1,26 @@
+package fredboat.audio;
+
+import java.util.HashMap;
+import net.dv8tion.jda.player.MusicPlayer;
+
+public class PlayerRegistry {
+    
+    private static HashMap<String, MusicPlayer> registry = new HashMap<>();
+    
+    public static void put(String k, MusicPlayer v){
+        registry.put(k, v);
+    }
+    
+    public static MusicPlayer get(String k){
+        MusicPlayer player = registry.get(k);
+        if (player == null){
+            player = new MusicPlayer();
+        }
+        return player;
+    }
+    
+    public static MusicPlayer remove(String k){
+        return registry.remove(k);
+    }
+    
+}
---
+++ src/main/java/fredboat/command/music/PlayCommand.java
@@ -0,0 +1,72 @@
+package fredboat.command.music;
+
+import fredboat.audio.PlayerRegistry;
+import fredboat.commandmeta.Command;
+import fredboat.commandmeta.MessagingException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.dv8tion.jda.Permission;
+import net.dv8tion.jda.entities.Guild;
+import net.dv8tion.jda.entities.Message;
+import net.dv8tion.jda.entities.SelfInfo;
+import net.dv8tion.jda.entities.TextChannel;
+import net.dv8tion.jda.entities.User;
+import net.dv8tion.jda.entities.VoiceChannel;
+import net.dv8tion.jda.managers.AudioManager;
+import net.dv8tion.jda.player.MusicPlayer;
+import net.dv8tion.jda.player.source.RemoteSource;
+import net.dv8tion.jda.utils.PermissionUtil;
+
+public class PlayCommand extends Command {
+
+    @Override
+    public void onInvoke(Guild guild, TextChannel channel, User invoker, Message message, String[] args) {
+        SelfInfo self = guild.getJDA().getSelfInfo();
+
+        //Check that we are in the same voice channel
+        if (guild.getVoiceStatusOfUser(invoker).getChannel() != guild.getVoiceStatusOfUser(self).getChannel()) {
+            joinChannel(guild, self, invoker);
+        }
+
+        //Now we will either have thrown an exception or be in the same channel
+        MusicPlayer player = PlayerRegistry.get(guild.getId());
+        AudioManager manager = guild.getAudioManager();
+        manager.setSendingHandler(player);
+        
+        RemoteSource source = new RemoteSource(args[1]);
+        player.getAudioQueue().add(source);
+        player.play();
+    }
+
+    public void joinChannel(Guild guild, SelfInfo self, User usr) {
+        VoiceChannel targetChannel = guild.getVoiceStatusOfUser(usr).getChannel();
+
+        if (!guild.getVoiceStatusOfUser(usr).inVoiceChannel()) {
+            throw new MessagingException("You must join a voice channel first.");
+        }
+
+        if (guild.getVoiceStatusOfUser(self).inVoiceChannel()) {
+            throw new MessagingException("I need to leave my current channel first.");
+        }
+
+        if (PermissionUtil.checkPermission(self, Permission.VOICE_CONNECT, targetChannel) == false) {
+            throw new MessagingException("I am not permitted to connect to that voice channel.");
+        }
+
+        if (PermissionUtil.checkPermission(self, Permission.VOICE_SPEAK, targetChannel) == false) {
+            throw new MessagingException("I am not permitted to play music in that voice channel.");
+        }
+
+        AudioManager manager = guild.getAudioManager();
+        manager.openAudioConnection(targetChannel);
+        while (manager.isAttemptingToConnect()) {
+            synchronized (this) {
+                try {
+                    wait(100);
+                } catch (InterruptedException ex) {
+                }
+            }
+        }
+    }
+
+}

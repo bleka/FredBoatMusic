8f7d40e524fd16d38b225eaa6d264fd06fbf1d52
Frederikam <frogkr@gmail.com>
2016-08-23 18:03:11 +0200

    Added function to tell if user is bot commander

--- src/main/java/fredboat/util/DiscordUtil.java
+++ src/main/java/fredboat/util/DiscordUtil.java
@@ -1,8 +1,10 @@
 package fredboat.util;
 
 import fredboat.MusicFredBoat;
+import java.util.List;
 import net.dv8tion.jda.JDA;
 import net.dv8tion.jda.entities.Guild;
+import net.dv8tion.jda.entities.Role;
 import net.dv8tion.jda.entities.User;
 
 public class DiscordUtil {
@@ -11,6 +13,18 @@ public class DiscordUtil {
         JDA jda = guild.getJDA();
         User other = jda.getUserById(MusicFredBoat.OTHER_BOT_ID);
         return guild.isMember(other);
+    }
+    
+    public static boolean isUserBotCommander(Guild guild, User user){
+        List<Role> roles = guild.getRolesForUser(user);
+        
+        for(Role r : roles){
+            if(r.getName().equals("Bot Commander")){
+                return true;
+            }
+        }
+        
+        return false;
     }
 
 }
